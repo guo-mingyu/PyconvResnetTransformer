de2e6f126a64a3a8b0ffe4a7ac5aa8e9da030116
ho <meiukaku@gmail.com>
2023-05-15 15:31:59 +0900

    fig datasets

--- datasets/coco.py
+++ datasets/coco.py
@@ -153,8 +153,8 @@ def build(image_set, args):
     #    "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
     #}
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'detr_train.json'),
-        "val": (root / "val2017", root / "annotations" / f'detr_val.json'),
+        "train": (root / "train2017", root / "annotations" / f'selected_train.json'),
+        "val": (root / "val2017", root / "annotations" / f'selected_val.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
--- run-detr.sh
+++ run-detr.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --coco_path /datasets/path/to/coco
+python -m torch.distributed.launch --use_env main.py --coco_path ./datasets/path/to/coco
