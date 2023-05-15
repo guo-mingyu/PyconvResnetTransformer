637291a7f787c280ed79d105ff95c616c3e5457c
ho <meiukaku@gmail.com>
2023-05-15 13:13:03 +0900

    fig datasets

--- run-detr.sh
+++ run-detr.sh
@@ -1,2 +1,1 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py \
-    --coco_path /dataset/path/to/coco
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --coco_path /datasets/path/to/coco
