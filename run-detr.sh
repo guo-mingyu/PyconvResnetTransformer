6979b5f9823e22176d8e6645dbfae0f51441f70e
ho <meiukaku@gmail.com>
2023-06-01 13:04:10 +0900

    fig datasets

--- run-detr.sh
+++ run-detr.sh
@@ -2,4 +2,9 @@ python -m torch.distributed.launch --use_env main.py \
     --dataset_file coco \
     --coco_path ./datasets/path/to/coco \
     --output_dir ./datasets/path/to/output \
-    --epochs 10
+    --epochs 10 \
+    --lr 1e-2 \
+    --weight_decay 1e-2 \
+    --lr_drop 100 \
+    --batch_size 4
+
